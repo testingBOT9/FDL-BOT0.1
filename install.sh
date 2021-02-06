69a161d284e52a25e2aef5be1af35818642fbacf
フェブリアン <75772930+testingBOT9@users.noreply.github.com>
2021-02-06 10:56:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-pkg update
-pkg upgrade
-pkg install wget
-pkg install ffmpeg
-pkg install nodejs
-npm install
-npm run start
+apt upgrade
+apt install
+apt install wget -y
+apt install ffmpeg -y
+apt install nodejs -y
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm update
